1882b75b70eb438249a5df8db454ac2fab2fab1b
Gabriel Fanha <gabrielfanha@alunos.utfpr.edu.br>
2024-10-21 15:32:16 -0300

    Primeiro Commit

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"encoding/binary"
+	"bytes"
+	"encoding/gob"
 	"fmt"
 	"log"
 	"net"
@@ -15,116 +16,27 @@ const (
 )
 
 type MetaData struct {
-	name     string
-	fileSize uint32
-	reps     uint32
-}
-
-func sendFile(path string, conn *net.UDPConn) {
-	file, err := os.OpenFile(path, os.O_RDONLY, 0755)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	header := prepareMetadata(file)
-
-	dataBuffer := make([]byte, 1014)
-
-	// Start (all 1s) - 1 byte, reps - 4 bytes, lengthofname - 4 bytes, name - `lengthofname` bytes, End (all 0s) - 1 byte;
-	headerBuffer := []byte{1}
-
-	// Start (all 0s) - 1 byte, Segment number - 4 bytes, lengthofdata - 4 bytes, Data - `lengthofdata` bytes, End (all 1s) - 1 byte
-	segmentBuffer := []byte{0}
-
-	// Temporary buffer for uint32
-	temp := make([]byte, 4)
-
-	// Temporary buffer for responses received
-	received := make([]byte, 100)
-
-	for i := 0; i < int(header.reps); i++ {
-		n, _ := file.ReadAt(dataBuffer, int64(i*1014))
-
-		if i == 0 {
-			// Number of segments
-			binary.BigEndian.PutUint32(temp, header.reps)
-			headerBuffer = append(headerBuffer, temp...)
-
-			// Length of name
-			binary.BigEndian.PutUint32(temp, uint32(len(header.name)))
-			headerBuffer = append(headerBuffer, temp...)
-
-			// Name
-			headerBuffer = append(headerBuffer, []byte(header.name)...)
-
-			headerBuffer = append(headerBuffer, 0)
-
-			_, err := conn.Write(headerBuffer)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			_, err = conn.Read(received)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			fmt.Println(string(received))
-		}
-
-		// Segment number
-		binary.BigEndian.PutUint32(temp, uint32(i))
-		segmentBuffer = append(segmentBuffer, temp...)
-
-		// Length of data
-		binary.BigEndian.PutUint32(temp, uint32(n))
-		segmentBuffer = append(segmentBuffer, temp...)
-
-		// Data
-		segmentBuffer = append(segmentBuffer, dataBuffer[:n]...)
-
-		segmentBuffer = append(segmentBuffer, 1)
-
-		_, err = conn.Write(segmentBuffer)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		_, err = conn.Read(received)
-		fmt.Println(string(received))
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// Reset segment buffer
-		segmentBuffer = []byte{0}
-	}
-}
-
-func prepareMetadata(file *os.File) MetaData {
-	fileInfo, err := file.Stat()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	size := fileInfo.Size()
-
-	header := MetaData{
-		name:     file.Name(),
-		fileSize: uint32(size),
-		reps:     uint32(size/1014) + 1,
-	}
-
-	return header
+	Name     string
+	FileSize int64
+	Reps     int32
+	Data     []byte
 }
 
 func main() {
+	request := make([]byte, 1024)
+
+	request = []byte("Hello from client")
+
 	udpServer, err := net.ResolveUDPAddr(TYPE, HOST+":"+PORT)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	file, errorFile := os.Create(`C:\Users\gfanha\Documents\testeUDP\` + "Escala-Controle-Julho (11).xls")
+	if errorFile != nil {
+		log.Fatal(errorFile)
+	}
+	defer file.Close()
 
 	conn, err := net.DialUDP(TYPE, nil, udpServer)
 	if err != nil {
@@ -132,14 +44,46 @@ func main() {
 	}
 	defer conn.Close()
 
-	sendFile(`C:\Users\gabri\Downloads\dotnet-runtime-8.0.8-win-x86.exe`, conn)
+	_, err = conn.Write(request)
 
 	received := make([]byte, 1024)
 
-	_, err = conn.Read(received)
-	if err != nil {
-		log.Fatal(err)
-	}
+	dec := gob.NewDecoder(bytes.NewReader(received))
+	var q MetaData
 
-	fmt.Println(string(received))
+	for i := 0; ; i++ {
+		_, err = conn.Read(received)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if len(received) > 0 {
+			errorDec := dec.Decode(&q)
+			if errorDec != nil {
+				log.Fatal(errorDec)
+			}
+			_, err2 := file.WriteAt(q.Data, int64(i*900))
+			if err2 != nil {
+				return
+			}
+			fmt.Println(q)
+			if q.Reps == 0 {
+				break
+			}
+
+		}
+	}
+}
+func AppendFile() {
+	file, err := os.OpenFile(`C:\Users\gfanha\Documents\testeUDP\`+"Escala-Controle-Julho (11).xls", os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatalf("failed opening file: %s", err)
+	}
+	defer file.Close()
+
+	len, err := file.WriteString(" The Go language was conceived in September 2007 by Robert Griesemer, Rob Pike, and Ken Thompson at Google.")
+	if err != nil {
+		log.Fatalf("failed writing to file: %s", err)
+	}
+	fmt.Printf("\nLength: %d bytes", len)
+	fmt.Printf("\nFile Name: %s", file.Name())
 }
