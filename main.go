3b4bc74b5e3a0a9571ac008684c836c7a3d3ee90
Gabriel Fanha <gabrielfanha@alunos.utfpr.edu.br>
2024-11-12 18:04:31 -0300

    Primeiro Commit

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"sync"
 )
 
 const (
@@ -32,7 +33,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	file, errorFile := os.Create(`C:\Users\gfanha\Documents\testeUDP\` + "Escala-Controle-Julho (11).xls")
+	file, errorFile := os.Create(`C:\Users\gfanha\Documents\testeUDP\` + "excelPlantonistas.xlsx")
 	if errorFile != nil {
 		log.Fatal(errorFile)
 	}
@@ -46,31 +47,44 @@ func main() {
 
 	_, err = conn.Write(request)
 
-	received := make([]byte, 1024)
-
-	dec := gob.NewDecoder(bytes.NewReader(received))
-	var q MetaData
-
-	for i := 0; ; i++ {
-		_, err = conn.Read(received)
+	data := make(chan byte)
+	done := make(chan bool)
+	wg := sync.WaitGroup{}
+	var finish bool = false
+	for !finish {
+		received := make([]byte, 1024)
+		_, _, err = conn.ReadFromUDP(received)
 		if err != nil {
 			log.Fatal(err)
 		}
-		if len(received) > 0 {
-			errorDec := dec.Decode(&q)
-			if errorDec != nil {
-				log.Fatal(errorDec)
-			}
-			_, err2 := file.WriteAt(q.Data, int64(i*900))
-			if err2 != nil {
-				return
-			}
-			fmt.Println(q)
-			if q.Reps == 0 {
-				break
-			}
-
-		}
+		wg.Add(1)
+		go handleIncomingResponse(received, data, &wg, &finish)
+	}
+	//if len(received) > 0 {
+	//	errorDec := dec.Decode(&q)
+	//	if errorDec != nil {
+	//		log.Fatal(errorDec)
+	//	}
+	//	_, err2 := file.WriteAt(q.Data, int64(i*900))
+	//	if err2 != nil {
+	//		return
+	//	}
+	//	fmt.Println(q)
+	//	if q.Reps == 0 {
+	//		break
+	//	}
+	//
+	//}
+	go writeToFile(data, done)
+	go func() {
+		wg.Wait()
+		close(data)
+	}()
+	d := <-done
+	if d {
+		fmt.Println("File written successfully")
+	} else {
+		fmt.Println("File not written")
 	}
 }
 func AppendFile() {
@@ -86,4 +100,48 @@ func AppendFile() {
 	}
 	fmt.Printf("\nLength: %d bytes", len)
 	fmt.Printf("\nFile Name: %s", file.Name())
+}
+
+func handleIncomingResponse(buffer []byte, data chan byte, wg *sync.WaitGroup, finish *bool) {
+	dec := gob.NewDecoder(bytes.NewReader(buffer))
+	var q MetaData
+	errorDec := dec.Decode(&q)
+	if errorDec != nil {
+		log.Fatal(errorDec)
+	}
+	fmt.Printf("Received: %v FileName: %v - Part: %d\n  Data = %v\n", q, q.Name, q.Reps, q.Data)
+	fmt.Printf("Data Size: %d\n", len(q.Data))
+	if len(q.Data) > 0 {
+		for _, d := range q.Data {
+			data <- d
+		}
+	}
+	wg.Done()
+	if q.Reps == 0 {
+		fmt.Println("Arquivo 0")
+		*finish = true
+	}
+}
+
+func writeToFile(data chan byte, done chan bool) {
+	f, err := os.Create("concurrent")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for d := range data {
+		_, err = fmt.Fprintln(f, d)
+		if err != nil {
+			fmt.Println(err)
+			f.Close()
+			done <- false
+			return
+		}
+	}
+	err = f.Close()
+	if err != nil {
+		log.Fatal(err)
+		done <- false
+		return
+	}
+	done <- true
 }
